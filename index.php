c89be3aa2beb8282736cfd7a7730615613600ec8
wsl <elkhailiissam0@gmail.com>
2025-03-02 09:19:25 +0100

    repush

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="author" content="issamelkhaili">
-  <meta name="description" content="ISSNote - A simple and efficient notepad for taking and managing notes.">
-  <meta name="keywords" content="notepad, notes, ISSNote, text editor, writing">
-  <title>ISSNote - Your Personal Notepad</title>
-  <link rel="stylesheet" href="styles.css">  <link rel="icon" href="favicon.ico" type="image/x-icon"> 
-</head>
-<header>
-<?php include "header.html"?>
-</header>
-<body>
-<?php include "body.html"?>
-</body>
+<?php
+include "header.html"
+?>
