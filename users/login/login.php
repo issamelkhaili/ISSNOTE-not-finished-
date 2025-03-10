aebd007801163033ff70c853e57018067568778a
wsl <elkhailiissam0@gmail.com>
2025-03-10 17:43:19 +0100

    O login W added H

--- users/login/login.php
+++ users/login/login.php
@@ -1,3 +1,29 @@
 <?php
-echo "i am here";
+$database = file("../database/database.txt");
+$userid = 0;
+$i;
+if(!$_POST)
+    exit;
+while((strpos($database[$userid], $_POST['email']) === false) && ($database[$userid] != "_______DATABSE_END________"))
+    $userid++;
+if(!strpos($database[$userid],$_POST[email]))
+{
+    echo "User Not Found";
+    exit;
+}
+$i = strpos($database[$userid],$_POST[email]);
+$data = explode("|",$database[$userid]);
+$email = $data[0];
+$password = $data[1];
+$username = $data[2];
+if($_POST[password] != $password)
+{
+    echo "Incorrect Password";
+    exit;
+}
+else
+{
+    echo "Welcome Back $username !";
+    exit;
+}
 ?>
